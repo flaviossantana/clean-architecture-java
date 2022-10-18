6fdb75c65e78a697761d9b12c86dd0edb29bb300
flavio.santana <9090flavio@gmail.com>
2022-10-17 21:48:25 -0300

    feat: value objects

--- src/main/java/com/school/clear/model/Email.java
+++ src/main/java/com/school/clear/model/Email.java
@@ -4,4 +4,16 @@ public class Email {
 
     private String endereco;
 
+    public Email(String endereco) {
+        if (isEmailInvalido(endereco)) {
+            throw new IllegalArgumentException("Endereço de email inválido");
+        }
+
+        this.endereco = endereco;
+    }
+
+    private static boolean isEmailInvalido(String endereco) {
+        return endereco == null ||
+                !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    }
 }

8d2c358fa4e2076bf4d7f8d85b50b0d0f80c1d96
flavio.santana <9090flavio@gmail.com>
2022-10-19 14:52:58 -0300

    feat: serviço de infraestrutura

---
+++ src/main/java/com/school/clear/infra/aluno/CifradorSenhaMD5.java
@@ -0,0 +1,31 @@
+package com.school.clear.infra.aluno;
+
+import com.school.clear.domain.aluno.CifradorSenha;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class CifradorSenhaMD5 implements CifradorSenha {
+
+    @Override
+    public String cifrarSenha(String senha) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(senha.getBytes());
+            byte[] bytes = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02X", 0xFF & b));
+            }
+            return sb.toString();
+        }catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Erro ao cifrar senha");
+        }
+    }
+
+    @Override
+    public boolean validarSenhaCifrada(String senhaCifrada, String senha) {
+        return senhaCifrada.equals(cifrarSenha(senha));
+    }
+
+}
